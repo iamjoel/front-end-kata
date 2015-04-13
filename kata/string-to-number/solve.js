d94799984cd39d07d407f4d067fbbacbd9f92bb4
金伟强 <iamjoel007@gmail.com>
2015-04-13 11:26:22 +0800

    enchance template

--- kata/string-to-number/solve.js
+++ kata/string-to-number/solve.js
@@ -1,7 +1,7 @@
-(function() {
+(function(global) {
     var stringToNumber = function(str) {
         return Number(str);
     }
 
-    this.stringToNumber = stringToNumber;// 暴露到全局作用域。是为了测试。
+    global.stringToNumber = stringToNumber;// 暴露到全局作用域。是为了测试。
 })(this);
--- kata/template/README.md
+++ kata/template/README.md
@@ -1,1 +1,8 @@
-# 
+# []()
+## Description
+
+
+## Examples
+```
+
+```
