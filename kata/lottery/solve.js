15f7e002abcd8f606dc66cdd721641eb6d3b9e3d
金伟强 <iamjoel007@gmail.com>
2017-01-05 10:13:47 +0800

    adds

--- kata/lottery/solve.js
+++ kata/lottery/solve.js
@@ -1,4 +1,10 @@
 var names = ['路飞', '香克斯', '白胡子', '娜美', '乔巴', '乌所普']
+var longName = [...names, ...names]
+longName = [...longName, ...longName, ...longName]
+longName = [...longName, ...longName, ...longName]
+longName = [...longName, ...longName, ...longName]
+const time = 1000000 // 十万次
+const resNum = 3
   // 解法1
 ; {
   var lottery = (names, num) => {
@@ -10,10 +16,15 @@ var names = ['路飞', '香克斯', '白胡子', '娜美', '乔巴', '乌所普'
     }
     return res
   }
-
-  console.log(lottery(names, 3))
-  console.log(lottery(names, 3))
-  console.log(lottery(names, 3))
+  benchmark(() => {
+    lottery(names, resNum)
+  }, time, '方法1 短数组')
+  benchmark(() => {
+    lottery(longName, resNum)
+  }, time, '方法1 长数组')
+  // console.log(lottery(names, 3))
+  // console.log(lottery(names, 3))
+  // console.log(lottery(names, 3))
 }
 
 // 解法2
@@ -26,7 +37,30 @@ var names = ['路飞', '香克斯', '白胡子', '娜美', '乔巴', '乌所普'
     return names.slice(0, 3)
   }
 
-  console.log(lottery(names, 3))
-  console.log(lottery(names, 3))
-  console.log(lottery(names, 3))
+  benchmark(() => {
+    lottery(names, resNum)
+  }, time, '方法2 短数组')
+  benchmark(() => {
+    lottery(longName, resNum)
+  }, time, '方法2 长数组')
+
+  // console.log(lottery(names, 3))
+  // console.log(lottery(names, 3))
+  // console.log(lottery(names, 3))
+}
+
+// 输出
+// 方法1 短数组花了2.089秒
+// 方法1 长数组花了45.353秒
+// 方法2 短数组花了2.436秒
+// 方法2 长数组花了119.705秒
+
+function benchmark(fn, time, name) {
+  var start = Date.now()
+  for(var i = 0; i < time; i++) {
+    fn()
+  }
+  var end = Date.now()
+  var useTime = (end - start) / 1000
+  console.log(`${name}花了${useTime}秒`)
 }
